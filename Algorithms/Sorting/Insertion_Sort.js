f147349a2121f6df16b3ed5524d4976ff04708aa
Rajatm544 <rajatm544@gmail.com>
2020-08-31 19:14:23 +0530

    fix my approach of insertion sort

--- Algorithms/Sorting/Insertion_Sort.js
+++ Algorithms/Sorting/Insertion_Sort.js
@@ -1,10 +1,11 @@
-// My approach, doesn't work
+// My approach
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
+        // Start comparing current element with every element before it
         for (let j = i - 1; j > -1; j--) {
-            if (arr[i] < arr[j]) {
-                [arr[j], arr[i]] = [arr[i], arr[j]];
-                break;
+            // Swap elements as required
+            if (arr[j + 1] < arr[j]) {
+                [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
             }
         }
     }
